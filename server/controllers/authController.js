b80d164d74de87d8c778992eddb52fa409bc3acb
Harsh Prajapati <golaharsh2001@gmail.com>
2023-08-27 23:31:55 +0530

    setted up with authentication system

---
+++ server/controllers/authController.js
@@ -0,0 +1,82 @@
+import bcrypt from "bcrypt";
+import generateToken from "../config/generateToken.js";
+import User from "../models/userModel.js";
+
+/* REGISTER USER */
+
+export const register = async (req, res) => {
+    try {
+        const { name, email, username, password } = req.body;
+        if (!name || !email || !password || !username) {
+            res.status(400);
+            throw new Error("Please Enter all the Fields");
+        }
+
+        const userExists = await User.findOne({
+            $or: [{ email: email }, { username: username }],
+        });
+
+        if (userExists) {
+            res.status(400);
+            throw new Error("User already Exists");
+        }
+
+        const salt = await bcrypt.genSalt();
+
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            name,
+            email,
+            username,
+            password: passwordHash,
+        });
+        const savedUser = await newUser.save();
+        console.log(savedUser);
+        res.status(201).json(savedUser);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+/* LOGIN USER */
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            res.status(400);
+            throw new Error("Please Enter all the Fields");
+        }
+
+        const user = await User.findOne({ email: email });
+
+        if (!user) {
+            return res.status(400).json({ msg: "User dose not exist." });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ msg: "Invalid credentials" });
+        }
+        const token = generateToken(user._id);
+
+        res.status(200).json({
+            token: token,
+            username: user.username,
+            name: user.name,
+            email: user.email,
+            userPic: user.userPic,
+            following: user.following,
+            followers: user.followers,
+            github: user.github,
+            linkedin: user.linkedin,
+            joinedCommunities: user.joinedCommunities,
+            joinedCourses: user.joinedCourses,
+        });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
---
+++ server/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+
+/* CONFIGURATIONS */
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(bodyParser.json({ limit: "20mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
+app.use(cors());
+
+/* Database Connection */
+
+connectDB();
+
+/* ROUTES */
+
+app.use("/api/auth", authRoutes);
+
+/* PORT */
+
+const PORT = process.env.PORT || 4142;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+    res.send("Api is Working");
+});
